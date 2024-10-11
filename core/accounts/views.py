5c4a060d2ff06a2839f3da4c5bb612689eb6eac6
mopiry <mopiry@gmai.com>
2024-10-11 17:01:14 +0330

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth import views as auth_views
-# from django.contrib.auth.views import (
-#      PasswordResetView, PasswordResetDoneView,
-#      PasswordResetConfirmView, PasswordResetCompleteView,
-#      PasswordChangeView, PasswordChangeDoneView, LoginView)
-
 from accounts.forms import AuthenticationForm
 from django.urls import reverse_lazy
+from .models import User
+from django.contrib import messages
+from django.shortcuts import render
 # from django.contrib.messages.views import SuccessMessageMixin
 
 class LoginView(auth_views.LoginView):
@@ -26,6 +24,13 @@ class CustomPasswordResetView(auth_views.PasswordResetView):
     success_url = reverse_lazy('accounts:password_reset_done')
 
 
+    # overriding form_valid for invalid email addresses
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')       
+        if not User.objects.filter(email=email).exists():
+            messages.error(self.request, 'ایمیل وارد شده در سیستم موجود نیست.')
+            return render(self.request, self.template_name, {'form': form})د
+        return super().form_valid(form)
 
 class CustomPasswordResetDoneView(auth_views.PasswordResetDoneView):
     template_name = 'accounts/password_reset_done.html'
