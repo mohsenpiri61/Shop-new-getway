0c45ded2bd7f353e01d30029f035690846f12937
mopiry <mopiry@gmai.com>
2024-10-28 19:03:30 +0330

    cart update

--- core/cart/views.py
+++ core/cart/views.py
@@ -13,8 +13,7 @@ class SessionAddProductView(View):
         if product_id and ProductModel.objects.filter(id=product_id, status=ProductStatusType.publish.value).exists():
 
             cart.add_product(product_id)
-        if request.user.is_authenticated:
-            cart.merge_session_cart_in_db(request.user)
+
         return JsonResponse({"cart": cart.get_cart_dict(), "total_quantity": cart.get_total_quantity()})
 
 
@@ -25,8 +24,7 @@ class SessionRemoveProductView(View):
         product_id = request.POST.get("product_id")
         if product_id:
             cart.remove_product(product_id)
-        if request.user.is_authenticated:
-            cart.merge_session_cart_in_db(request.user)
+
         return JsonResponse({"cart": cart.get_cart_dict(), "total_quantity": cart.get_total_quantity()})
 
 
@@ -38,8 +36,7 @@ class SessionUpdateProductQuantityView(View):
         quantity = request.POST.get("quantity")
         if product_id and quantity:
             cart.update_product_quantity(product_id, quantity)
-        if request.user.is_authenticated:
-            cart.merge_session_cart_in_db(request.user)
+
         return JsonResponse({"cart": cart.get_cart_dict(), "total_quantity": cart.get_total_quantity()})
 
 
