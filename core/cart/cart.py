79dccf6920962a3ece72847caf8eabb2ceb8b322
mopiry <mopiry@gmai.com>
2024-11-02 22:29:46 +0330

    Update cart.py

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -7,6 +7,18 @@ class CartSession:
         self.session = session
         self._cart = self.session.setdefault("cart", {"items": []})
 
+    
+    def add_product(self, product_id):
+        for item in self._cart["items"]:
+            if product_id == item["product_id"]:
+                item["quantity"] += 1
+                break
+        else:
+            new_item = {"product_id": product_id, "quantity": 1}
+            self._cart["items"].append(new_item)
+        self.save()
+    
+    
     def update_product_quantity(self, product_id, quantity):
         for item in self._cart["items"]:
             if product_id == item["product_id"]:
@@ -15,6 +27,7 @@ class CartSession:
         else:
             return
         self.save()
+        
 
     def remove_product(self, product_id):
         for item in self._cart["items"]:
@@ -25,22 +38,16 @@ class CartSession:
             return
         self.save()
 
-    def add_product(self, product_id):
-        for item in self._cart["items"]:
-            if product_id == item["product_id"]:
-                item["quantity"] += 1
-                break
-        else:
-            new_item = {"product_id": product_id, "quantity": 1}
-            self._cart["items"].append(new_item)
-        self.save()
+
 
     def clear(self):
         self._cart = self.session["cart"] = {"items": []}
         self.save()
+        
 
     def get_cart_dict(self):
         return self._cart
+    
 
     def get_cart_items(self):
         for item in self._cart["items"]:
@@ -48,12 +55,15 @@ class CartSession:
             item.update({"product_obj": product_obj, "total_price": item["quantity"] * product_obj.get_price()})
 
         return self._cart["items"]
+    
 
     def get_total_payment_amount(self):
         return sum(item["total_price"] for item in self._cart["items"])
+    
 
     def get_total_quantity(self):
         return sum(item["quantity"] for item in self._cart["items"])
 
+    
     def save(self):
         self.session.modified = True
