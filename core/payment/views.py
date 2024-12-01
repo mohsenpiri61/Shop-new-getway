590518bf8681f9261f017d50b72e600913dbb962
mopiry <mopiry@gmai.com>
2024-12-01 08:42:13 +0330

    Update views.py

--- core/payment/views.py
+++ core/payment/views.py
@@ -13,37 +13,20 @@ class PaymentCallbackView(View):
         payment_number = full_path.rstrip('/').split('/')[-1]  # استخراج بخش آخر
         print(f"Payment Number: {payment_number}")  # نمایش در کنسول
         
-
-        # بازیابی اطلاعات پرداخت
         payment_obj = get_object_or_404(PaymentModel, payment_number=payment_number)
         
-        # مقداردهی به زرین پال و دریافت پاسخ از زرین پال به منظور اطمینان از موفقیت آمیز بودن پرداخت
+       
         sepal = SepalPaymentGateway() 
         response = sepal.payment_verify(int(payment_obj.amount), payment_obj.payment_number)
         
-        # استخراج داده‌های پاسخ
-        
         status_code = response.get("status", 3)
-        
-        
-        # ثبت اطلاعات پرداخت در مدل
         
         payment_obj.response_code = status_code
         payment_obj.status = PaymentStatusType.success.value if status_code in {
             100, 101} else PaymentStatusType.failed.value
         payment_obj.response_json = response
         payment_obj.save()
-        
-        # مشاهده اطلاعات نمونه در کنسول (payment_obj)
-        for field in payment_obj._meta.get_fields():
-            # فیلدهایی که مستقیم نیستند (یعنی رابطه معکوس هستند) را رد کن
-            if not field.concrete:
-                continue
-            field_name = field.name
-            field_value = getattr(payment_obj, field_name, None)
-            print(f"{field_name}: {field_value}")
 
-        # بروزرسانی وضعیت سفارش مرتبط
         order = OrderModel.objects.get(payment=payment_obj)
         order.status = OrderStatusType.PAID.value if status_code in {
             100, 101} else OrderStatusType.CANCELED.value
